6319abc30945944518f34143bfd6c2149d8d36c2
Patrick-Tomic <pat12tomic@gmail.com>
2024-03-26 23:37:02 -0400

    header functionality work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,35 @@
- import Header from "@/components/header";
+'use client'
+import Header from '@/components/header'
+import { useEffect } from 'react'
+export default function Home () {
+  useEffect(() => {
+    document.getElementById('brandHead')?.addEventListener('mouseover', () => {
+      document.querySelectorAll('#brandChild').forEach((child) => {
+        child.setAttribute('style', 'display:block;')
+      })
+    })
 
-export default function Home() {
- return(
+    document.getElementById('categoryHead')?.addEventListener('mouseover', () => {
+      document.querySelectorAll('#categoryChild').forEach((child) => {
+        child.setAttribute('style', 'display:block')
+      })
+
+      document.getElementById('catUL')?.addEventListener('mouseleave', () => {
+        document.querySelectorAll('#categoryChild').forEach((child) => {
+        child.setAttribute('style', 'display:hidden')
+        })
+      })
+
+      document.getElementById('brandUL')?.addEventListener('mouseleave', () => {
+        document.querySelectorAll('#brandChild').forEach((child) => {
+        child.setAttribute('style', 'display:hidden')
+        })
+      })
+    })
+  })
+  return (
   <>
   <Header />
   </>
- )
-   
+  )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,25 +1,46 @@
+/* eslint-disable import/no-absolute-path */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import Image from 'next/image'
-import xbn from '../xbn.png'
-interface Brands {
-  _id: string
-  name: string
-}
-export default async function Header () {    
-    const response = await fetch('http://localhost:3000/api/brands')
-    const data = await response.json()
-    const brands = data.brands
-    const brandListItems = brands.map((brand: any) => {
-        return ( 
-            <li>
-                {brand.name}
+import xbn from '/public/xbn.png'
+export default async function header () {
+  const brandResponse = await fetch('http://localhost:3000/api/brands')
+  const data = await brandResponse.json()
+  const brands = data.brands
+  const brandListItems = brands.map((brand: any) => {
+    return (
+            <li id='brandChild' className="hidden">
+                <a href="#">{brand.name}</a>
             </li>
-        )
-    })
-    
+    )
+  })
+  const categoryResponse = await fetch('http://localhost:3000/api/categories')
+  const dataB = await categoryResponse.json()
+  const categories = dataB.categories
+  const categoryListItems = categories.map((cat: any) => {
+    return (
+        <li id='categoryChild' className='hidden'>
+            <a href="#">{cat.type}</a>
+        </li>
+    )
+  })
   return (
-        <header className="flex">
-            {brandListItems}
+        <header className="flex border-2 border-black border-solid justify-around items-center">
+            <a href="#">Home</a>
+             <ul id='brandUL'>
+                <li id='brandHead'>Shop by Brand</li>
+                <div className='absolute'>
+                {brandListItems}
+                </div>
+             </ul>
+             <Image className="w-[12vw] h-[18vh]"
+              src={xbn} alt={''} />
+             <ul id='catUL'>
+                <li id='categoryHead'>Shop by Category</li>
+                <div className='absolute'>
+                {categoryListItems}
+                </div>
+             </ul>
+             <a href="#">Contact Us</a>
         </header>
   )
 }
