73ce0fd2835a36ca5813f453899276091d520397
Patrick-Tomic <pat12tomic@gmail.com>
2024-07-30 20:59:47 -0400

    alot of creation for address portion of setting page

--- src/app/settings/[id]/page.tsx
+++ src/app/settings/[id]/page.tsx
@@ -7,6 +7,8 @@ import Header from "@/components/header";
 export default function UserPage() {
     const [admin, setAdmin] = useState(false);
     const [error, setError] = useState("");
+    const [addressUpdate, setAddressUpdate] = useState(false);
+    const [errMessage, setErrMessage] = useState("");
     const [user, setUser] = useState({
         firstName: "",
         lastName: "",
@@ -22,7 +24,11 @@ export default function UserPage() {
         email: Yup.string(),
         password: Yup.string(),
         newPassword: Yup.string(),
-        address: Yup.array(),
+        address: Yup.string(),
+        city: Yup.string(),
+        state: Yup.string(),
+        zip: Yup.string(),
+        index: Yup.number(),
         payment: Yup.array(),
         purchaseHistory: Yup.array()
     })
@@ -74,6 +80,31 @@ const submitFormA2 = async(data: any) => {
         }
 
 }
+const submitFormB = async(data: any) => {
+    const id = localStorage.getItem('id')  
+    let obj
+    if(addressUpdate){
+          obj = {id:id , address: data.address, city: data.city, state: data.state, zip: data.zip, index: data.index}
+    }else{
+         obj = {id:id , address: data.address, city: data.city, state: data.state, zip: data.zip}
+    }
+    console.log(obj)
+        let formData = JSON.stringify(obj)
+        try {
+            const req = await fetch(`${process.env.NEXT_PUBLIC_backend_Link}update`  , {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+           const file = await req.json()
+           console.log(file)
+        }
+        catch(err: any){
+            setErrMessage(err)
+        }
+}
     useEffect(() => {
         (async() => {
            const id = localStorage.getItem("id")
@@ -104,7 +135,61 @@ const submitFormA2 = async(data: any) => {
         });
         });
     }, []);
-    const [errMessage, setErrMessage] = useState("");
+    const purchaseHistory = user.purchaseHistory.map((purchase: any) => {
+        const objects = purchase.order.map((item: any) => {
+            return(
+                <div>
+                    <div className="flex w-32">
+                        <p>{item.product}</p>
+                        <p>{item.brand}</p>
+                    </div>
+                    <div className="flex w-32">
+                        <p>{item.price}</p> -
+                        <p>{item.quantity}</p>
+                </div>
+                </div>
+            )
+        })
+        return(
+            <div className="flex">
+                <h1>{purchase.date}</h1>
+            <div id="items">
+                {objects}
+            </div>
+            <h1>Price: {purchase.price}</h1>
+            </div>
+        )
+    })
+    const billingInfo = user.address.map((address :any) => {
+        let index = -1
+        index++
+        return(
+            <div className="flex bg-[#FFFBD6] p-10 justify-around border-4 rounded-md border-solid border-[#353935]">
+                <input type="radio" name={`${address.street}`} value={index} />
+            <label htmlFor = {`${address.street}`}>
+                <div className="flex flex-col">
+                <h1 className="text-xl font-bold">{address.street}</h1>
+                <p className="font-semibold">{address.city}  {address.state},   {address.zip}</p>
+                </div>
+                
+            </label>
+            
+            <button className=" bg-green-500 w-[100px] h-[50px] rounded-md mt-[-10px] hover:bg-[#353935] transition-all ease-in-out hover:text-white" onClick={() => {
+                            document.getElementById('createBillInfo')?.setAttribute('style', 'display:flex');
+                            document.getElementById('addressInput')?.setAttribute('value', address.street);
+                            document.getElementById('cityInput')?.setAttribute('value', address.city);
+                            document.getElementById('stateInput')?.setAttribute('value', address.state);
+                            document.getElementById('zipInput')?.setAttribute('value', address.zip);
+                            setAddressUpdate(true);
+                            document.getElementById('indexInput')?.setAttribute('value', `${index}`);
+            }}
+            >Edit</button>
+            <button className=" bg-red-500 w-[100px] h-[50px] rounded-md mt-[-10px] hover:bg-[#353935] transition-all ease-in-out hover:text-white">Delete</button>
+            </div>
+        )
+        
+    })
+console.log(user.purchaseHistory)
     return(
         <>
         <Header />
@@ -112,7 +197,7 @@ const submitFormA2 = async(data: any) => {
             <div className="flex flex-col bg-[#353935] col-span-1 h-[100vh] text-white p-10">
                 <div id="userInfo" className="flex flex-col">
                     <h2 className="font-bold text-2xl">User Information</h2>
-                    <a href="#">Purchase History</a>
+                    <a href="#purchase">Purchase History</a>
                     <a href="#billing">Billing Information</a>
                     <a href="#payment">Payment Methods</a>
                 </div>
@@ -123,39 +208,73 @@ const submitFormA2 = async(data: any) => {
                     <a href="FAQ">FAQ</a>
                 </div>
             </div>
-            <div className="flex flex-col col-span-2 p-10" id="settingContent">
+            <div className="flex flex-col col-span-2 p-0" id="settingContent">
           {/* need to create second form of authentication through email */}
-            <div className="flex flex-col" id="UpdatePasswordDiv">
-                    <div className="flex justify-around w-[600px]">
+            <div className="flex flex-col border-b-4 border-solid border-black" id="UpdatePasswordDiv">
+                    <div className="flex justify-around w-[600px] p-10">
                         <h1 className=" font-bold text-2xl">Hello {user.firstName}</h1>
-                       
                         <button id="updateBtn" onClick={() => {
                             document.getElementById('updateBtn')?.setAttribute('style', 'display:none');
                             document.getElementById('passwordResetConfirm')?.setAttribute('style', 'display:block');
                
                             document.getElementById('password')?.removeAttribute('readOnly');
-                        }} className=" bg-[#F38015] w-[100px] h-[50px] rounded-md mt-1 hover:bg-[#353935] transition-all ease-in-out hover:text-white">
+                        }} className=" bg-[#F38015] w-[100px] h-[50px] rounded-md mt-[-10px] hover:bg-[#353935] transition-all ease-in-out hover:text-white">
                             Update Password
                         </button>
                         </div>
                         <div>
-                        <form action="" onSubmit={handleSubmit(submitFormA1)} className="flex flex-col " id="userInfoForm">
+                        <form action="" onSubmit={handleSubmit(submitFormA1)} className="flex flex-col p-5" id="userInfoForm">
                             
-                                <label htmlFor="email">Email</label>
-                                <input type="email" id="email" {...register('email')} value={user.email} readOnly/>
+                                <label className="text-xl font-bold" htmlFor="email">Email:</label>
+                                <input className="text-xl border-2 p-1 rounded-md" type="email" id="email" {...register('email')} value={user.email} readOnly/>
                                 <label htmlFor="password">Password</label>
-                                <input type="password" {...register('password')} id="password"  readOnly/>
-                                <button className="hidden border-2 border-black border-solid bg-green-500 w-[10vw]" type="submit" id="passwordResetConfirm">Enter</button>
+                                <input className="text-xl border-2 p-1 rounded-md" type="password" {...register('password')} id="password"  readOnly/>
+                                <button className="hidden w-[100px] h-[40px] rounded-md bg-green-500 font-bold hover:bg-[#353935] transition-all ease-in-out hover:text-white" type="submit" id="passwordResetConfirm">Enter</button>
                             </form>
                             <form action="" onSubmit={handleSubmit(submitFormA2)} id="updatePassword" className="hidden flex-col">
                                 <label htmlFor="resetPassword">Enter New Password</label>
-                                <input type="password"  {...register('newPassword')} id="newPassword" />
-                                <button type="submit" id="resetPasswordSubmit">Enter</button>
+                                <input className="text-xl border-2 p-1 rounded-md" type="password"  {...register('newPassword')} id="newPassword" />
+                                <button className="hidden w-[100px] h-[40px] rounded-md bg-green-500 font-bold hover:bg-[#353935] transition-all ease-in-out hover:text-white" type="submit" id="resetPasswordSubmit">Enter</button>
                             </form>
                             </div>
                 </div>
-            <div>
+            <div id="purchase">
+
+            </div>
+            <div className=" border-b-2 border-black border-solid p-10" id="billing">
+                        <div className="flex w-[600px] justify-around">
+                        <h1 className="font-bold text-2xl">Billing Information</h1>
+                        <button onClick={() => {
+                            document.getElementById('createBillInfo')?.setAttribute('style', 'display:flex');
+                        }} className=" bg-[#F38015] w-[100px] h-[50px] rounded-md mt-[-10px] hover:bg-[#353935] transition-all ease-in-out hover:text-white">Add Billing Option </button>
+                        </div>
+                        <div className="p-10" >
+                            {billingInfo}
+                        </div>
                         
+                    <form className="hidden flex-col" onSubmit={handleSubmit(submitFormB)} id='createBillInfo' action="">
+                    <label htmlFor="">Address</label>    
+                    <input type="text"  {...register('address')} id="addressInput" />
+                    <label htmlFor="">City</label>
+                    <input type="text" {...register('city')} id="cityInput" />
+                    <label htmlFor="">State</label>
+                    <input type="text" id="stateInput" {...register('state')} />
+                    <label htmlFor="">Zip Code:</label>
+                    <input type="text" {...register('zip')} id="zipInput" />
+                    <input type="number" className="hidden" {...register('index')} id="indexInput" />
+                    <div className="w-[250px] flex justify-around mt-5">
+                        <div>
+                    <button className=" w-[100px] h-[40px] rounded-md bg-green-500 font-bold hover:bg-[#353935] transition-all ease-in-out hover:text-white" type="submit" id="submitNewAddress">Enter</button>
+                    </div>
+                    <button className=" w-[100px] h-[40px] rounded-md bg-red-500 font-bold hover:bg-[#353935] transition-all ease-in-out hover:text-white" type="button" onClick={() => {
+                        document.getElementById('createBillInfo')?.setAttribute('style', 'display:none');
+                        document.getElementById('addressInput')?.setAttribute('value', '');
+                        document.getElementById('cityInput')?.setAttribute('value', '');
+                        document.getElementById('stateInput')?.setAttribute('value', '');
+                        document.getElementById('zipInput')?.setAttribute('value', '');
+                    }}>Cancel</button>
+                    </div>
+                    </form>
             </div>    
             <div id="adminDiv">
                     <div id="inventory">
@@ -171,9 +290,7 @@ const submitFormA2 = async(data: any) => {
                 <div id="userInfo">
 
                 </div>
-                <div id="billing">
-
-                </div>
+                
                 <div id="payment">
 
                 </div>
